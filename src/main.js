792c6a8466dcd1d0cd4f725d85f3213323b47c49
alessandro <146910739+Alessdev@users.noreply.github.com>
2024-05-03 21:25:40 -0500

    function generator added

--- src/main.js
+++ src/main.js
@@ -9,3 +9,45 @@ document.getElementById('id boton').addEventListener('click', function() {
     cartSidebar.classList.remove('open');
     
 })
+
+let products = [
+    {product:"disco-solido",
+        price: 10,
+        marca: "kingstom",
+        img: "images/img-1.jpg"	
+    },
+    {product:"laptop",
+        price: 100,
+        marca: "Asus",
+        img: "images/img-2.jpg"	
+    },
+    {product:"monitor", 
+        price: 2900,
+        marca: "Asus",
+        img: "images/img-3.jpg"	
+    },
+    {product:"audifonos",
+        price: 200,
+        marca: "hawuei",
+        img: "images/img-4.jpg"	
+    },
+    ];
+let imagenes = document.getElementById('images')
+
+ Generator = () => {
+   return (imagenes.innerHTML = products.map((x)=>{
+    return (`
+    <div class='contenedordeventa'>
+       <div class="item" id = "item">
+           <img src="images/img-7.jpg">
+       </div>
+       <div class="cuadrocompra" id = "cuadrocompra">
+           <p>Producto: Camisa</p>
+       </div>
+       <div class="cuadrocompra" id = "cuadrocompra">
+           <button>Producto: Camisa</button>
+       </div>
+    </div>`
+    )
+   }))};
+Generator(products)
